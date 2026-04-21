0bf993fc00f97f3e9adfbbee7dddf768e71170a6
TerraFlopp <150452607+TerraFlopp@users.noreply.github.com>
2026-04-21 18:51:45 +0200

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -31,7 +31,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "Alem Djibril — Video Editor & Developer" },
+      { title: "L'étoile montante du montage" },
       {
         name: "description",
         content:
